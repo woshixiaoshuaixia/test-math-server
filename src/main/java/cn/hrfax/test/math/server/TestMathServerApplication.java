643bf6f4ea269e900d6170438e455f82ab1392bb
jingfei <jingfei@hrfax.cn>
2025-04-25 10:38:20 +0800

    1, 测试文件

---
+++ src/main/java/cn/hrfax/test/math/server/TestMathServerApplication.java
@@ -0,0 +1,16 @@
+package cn.hrfax.test.math.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jingfei
+ */
+@SpringBootApplication
+public class TestMathServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMathServerApplication.class, args);
+    }
+
+}
